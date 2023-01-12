1bfe9085bf6bcae7164dda5639bece4c27a3824b
etienkh <74093435+etienkh@users.noreply.github.com>
2023-01-13 00:12:16 +0200

    commit 3 - branch test 2=4=

--- NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/NFP121_nib_2417n_2419n.java
+++ NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/NFP121_nib_2417n_2419n.java
@@ -17,7 +17,7 @@ public class NFP121_nib_2417n_2419n {
     public static void main(String[] args) {
         // TODO code application logic here
        if(1 == 1){
-           System.out.println("abc");
+           System.out.println("abccc");
        }
 
     }
