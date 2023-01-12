ae1c7e40ab1cd75aa7722e41796cc3d50a2c8d9e
etienkh <74093435+etienkh@users.noreply.github.com>
2023-01-12 23:21:10 +0200

    commit 1 - Create Repository/Project

---
+++ NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/NFP121_nib_2417n_2419n.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nfp121_nib_2417n_2419n;
+
+/**
+ *
+ * @author User
+ */
+public class NFP121_nib_2417n_2419n {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
