5d4d01f171da959e50203b609eb600c94ffcf339
etienkh <74093435+etienkh@users.noreply.github.com>
2023-01-27 00:10:54 +0200

    Commit 1 - Create Login Singleton class and fix some issues in Factory Class

--- NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/Model/Person.java
+++ NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/Model/Person.java
@@ -15,12 +15,14 @@ import java.io.ObjectInputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
- public class Person implements Serializable {
-    public int id ;
-    public  String username;
+public class Person implements Serializable {
+
+    public int id;
+    public String username;
     public String firstName;
     public String lastName;
     public String password;
+
     public ArrayList<Person> readAllPerson() {
         ArrayList<Person> list = new ArrayList<Person>();
         File file = new File("person");
@@ -35,22 +37,27 @@ import java.util.ArrayList;
         }
         return list;
     }
-       public Person(String username , String firstName,String lastName, String password) {
+
+    public Person(String username, String firstName, String lastName, String password) {
         this.username = username;
         this.firstName = firstName;
         this.lastName = lastName;
         this.password = password;
-        
-        if(readAllPerson().size() == 0){
+
+        if (readAllPerson().size() == 0) {
             id = 1;
+        } else {
+            id = readAllPerson().get(readAllPerson().size() - 1).id + 1;
         }
-        else{
-         id = readAllPerson().get(readAllPerson().size()-1).id  + 1;  
-        }
-        
     }
-       public String toString(){
-           return this.username+" "+this.firstName+" "+this.lastName+" "+this.password;
-       }
+
+    public Person(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String toString() {
+        return this.username + " " + this.firstName + " " + this.lastName + " " + this.password;
+    }
 
 }
