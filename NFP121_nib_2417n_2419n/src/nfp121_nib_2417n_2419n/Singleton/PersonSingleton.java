e75a41067a2af0dcdb41bc776a51a20ac09db065
etienkh <74093435+etienkh@users.noreply.github.com>
2023-02-19 14:58:53 +0200

    Fix singleton constructor

--- NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/Singleton/PersonSingleton.java
+++ NFP121_nib_2417n_2419n/src/nfp121_nib_2417n_2419n/Singleton/PersonSingleton.java
@@ -1,17 +1,18 @@
 package nfp121_nib_2417n_2419n.Singleton;
 
-import java.io.IOException;
-
 import nfp121_nib_2417n_2419n.Model.Person;
 
 public class PersonSingleton {
-    
-        private static Person person;
-        
-        public static Person getInstance(Person p){
-            if(person != null)
-                return person;
-            person = p;
+
+    private static Person person;
+
+    private PersonSingleton() {
+    }
+
+    public static Person getInstance(Person p) {
+        if (person != null)
             return person;
-        }
+        person = p;
+        return person;
+    }
 }
